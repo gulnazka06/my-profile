81dda87e9681f14b50cb98c5b789244b0d0c5da5
Ildar Timerbaev <dkildar@gmail.com>
2019-10-09 22:54:18 +0300

    Add server for docker

---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require('express')
+const app = express()
+const port = 4203
+
+app.use('/img', express.static('dist/img'));
+app.use('/css', express.static('dist/css'));
+app.use('/js', express.static('dist/js'));
+
+app.get('/', function(req, res){
+  res.sendfile(__dirname + '/dist/index.html');
+});
+app.get('/favicon.png', function(req, res){
+  res.sendfile(__dirname + '/dist/favicon.png');
+});
+
+app.listen(port, (err) => {
+  if (err) {
+    return console.log('something bad happened', err)
+  }
+  console.log(`server is listening on ${port}`)
+})
